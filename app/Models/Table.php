aca7654beb5f31b6962f6ef8b719762c263f2ae7
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-01-14 13:46:06 +0000

    add new table

--- app/Models/Table.php
+++ app/Models/Table.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Enums\TableStatus;
+use App\Enums\TableLocation;
 
 class Table extends Model
 {
     use HasFactory;
+    protected $fillable=['name','guest_number','status','location'];
+    protected $casts=[
+       'status'=>TableStatus::class,
+       'location'=>TableLocation::class
+   ];
 }
--- resources/views/admin/tables/create.blade.php
+++ resources/views/admin/tables/create.blade.php
@@ -7,42 +7,58 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-   <div class="flex  m-2 p-2">  
-    <a href="{{ route('admin.tables.index') }}"  class="px-4 py-2 bg-indigo-200 hover:bg-indigo-700  rounded-lg text-black">
-        Back
-    </a>
-   </div> 
-  <div class="m-2 p-2 bg-slate-100 rounded">
+            <div class="flex m-2 p-2">
+                <a href="{{ route('admin.tables.index') }}"
+                    class=" px-4 py-2 bg-indigo-200 hover:bg-indigo-300  rounded-lg text-black">Back</a>
+            </div>
+            <div class="m-2 p-2 bg-slate-100 rounded">
                 <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                    <form method="POST" action="{{ route('admin.categories.store') }}" enctype="multipart/form-data">
+                    <form method="POST" action="{{ route('admin.tables.store') }}">
                         @csrf
                         <div class="sm:col-span-6">
                             <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
                             <div class="mt-1">
                                 <input type="text" id="name" name="name"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
                             @error('name')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="sm:col-span-6">
-                            <label for="image" class="block text-sm font-medium text-gray-700"> Image </label>
+                            <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guest Number
+                            </label>
                             <div class="mt-1">
-                                <input type="file" id="image" name="image"
-                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror" />
+                                <input type="number" id="guest_number" name="guest_number"
+                                    class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
                             </div>
-                            @error('image')
+                            @error('guest_number')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
                         <div class="sm:col-span-6 pt-5">
-                            <label for="description" class="block text-sm font-medium text-gray-700">Description</label>
+                            <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
                             <div class="mt-1">
-                                <textarea id="description" rows="3" name="description"
-                                    class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md @error('name') border-red-400 @enderror"></textarea>
+                                <select id="status" name="status" class="form-multiselect block w-full mt-1">
+                                    @foreach (App\Enums\TableStatus::cases() as $status)
+                                        <option value="{{ $status->value }}">{{ $status->name }}</option>
+                                    @endforeach
+                                </select>
                             </div>
-                            @error('description')
+                            @error('status')
+                                <div class="text-sm text-red-400">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        <div class="sm:col-span-6 pt-5">
+                            <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+                            <div class="mt-1">
+                                <select id="location" name="location" class="form-multiselect block w-full mt-1">
+                                    @foreach (App\Enums\TableLocation::cases() as $location)
+                                        <option value="{{ $location->value }}">{{ $location->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            @error('location')
                                 <div class="text-sm text-red-400">{{ $message }}</div>
                             @enderror
                         </div>
@@ -56,4 +72,4 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-admin-layout>
