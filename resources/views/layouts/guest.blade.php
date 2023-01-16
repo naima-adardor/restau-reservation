12b03fb6a7b4af2d0b8e5ab212c54a6fe319196e
Adardor Naima <naima.adardor@edu.uiz.ac.ma>
2023-01-16 22:08:01 +0000

    some changes

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -50,6 +50,59 @@
                     href="{{route( 'menus.index') }}">Our Menu</a>
                     <a class="text-transparent bg-clip-text font-bold bg-blue-300  hover:text-blue-400"
                     href="{{route( 'reservations.step.one') }}" >Make Reservation</a>
+                    @guest
+                    @if (Route::has('register')||Route::has('login'))
+                    
+                    <button class="text-transparent bg-clip-text font-bold bg-blue-900  hover:text-blue-700">
+                    <span>Connect</span>
+
+                           
+                             <x-dropdown-link class="text-transparent bg-clip-text font-bold bg-blue-300  hover:text-blue-400" href="{{ route('login') }}" >
+                                    {{ __('Login') }}
+                                              {{ __('Register') }}
+                                </x-dropdown-link>
+
+                               
+                    </button>
+                      
+
+
+
+               @endif
+               @else
+
+                      <div @click.away="open = false" class="relative" x-data="{ open: false }">
+                    <button @click="open = !open"
+                        class="text-transparent bg-clip-text font-bold bg-blue-900  hover:text-blue-700">
+                        <span>{{ Auth::user()->name }}</span>
+                        <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}"
+                            class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1">
+                            <path fill-rule="evenodd"
+                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <div x-show="open" x-transition:enter="transition ease-out duration-100"
+                        x-transition:enter-start="transform opacity-0 scale-95"
+                        x-transition:enter-end="transform opacity-100 scale-100"
+                        x-transition:leave="transition ease-in duration-75"
+                        x-transition:leave-start="transform opacity-100 scale-100"
+                        x-transition:leave-end="transform opacity-0 scale-95"
+                        class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
+                        <div class="px-2 py-2 bg-white rounded-md shadow dark:bg-gray-700">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+
+                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                this.closest('form').submit();"
+                                    class="block px-1 w-full py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-blue-300 focus:text-gray-900  focus:outline-none focus:shadow-outline">
+                                    {{ __('LogOut') }}
+                                </x-dropdown-link>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                @endguest
     
                 </div>
             </nav>
